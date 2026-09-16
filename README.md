5e261c466e098534bd2c2e7f58c9d590e795a222
vicbro <vicbro@hotmail.no>
2026-09-16 12:02:24 +0200

    Arbeider videre med oppgave-3. Har nå gjort ferdig funksjonen for å legge inn en ny dato, og main() validerer funksjonen slik at den printer at det er feil ved invalid format eller input.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Victoria Louise Brown
+
+## Oppgave 2:
+Jeg valgte å strukturere dataene mine innenfor en liste som heter all_sessions.
+Innenfor denne listen har jeg 5 dictionaries for studie økter.
+Når brukeren da lager en ny økt, blir den lagret som en ny dictionary innenfor all_sessions listen.
--- oppgave-3.py
+++ oppgave-3.py
@@ -1,22 +1,38 @@
 import datetime as dt
+from datetime import datetime
 
 # Ikke forventet en meny, bare funksjoner
 
 def new_date(date):
-    value = input("Date? ")
-
-    dt.datetime.strptime(value,
-                     "%d.%m.%Y")
-    print(date)
-    return date
+    try:
+        date = dt.datetime.strptime(date,
+            "%d.%m.%Y")
+        return date
+    except ValueError:
+        print("Invalid date format")
+        return None
 
 def end_date(start_time, minutes):
-    end = start_time - minutes
-    return end
+    print("In progress")
 
 def remaining_days(date1, date2):
-    remaining = date2 - date1
-    return remaining
+    if date2 > date1:
+        remaining = date2 - date1
+        return remaining.date
+    else:
+        remaining = date1 - date2
+        return remaining.date
 
-def chronological_list():
+def chronological_list(list_of_dates):
     print("In progress")
+
+def main():
+    while True:
+        first_date = new_date(input("What is the date? "))
+        if first_date is not None:
+            print(first_date)
+            break
+        else:
+            pass
+if __name__ == "__main__":
+    main()
