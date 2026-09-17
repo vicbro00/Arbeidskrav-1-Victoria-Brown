35f20606a4f72bd872f34642c7c5a2ce9e018352
vicbro <vicbro@hotmail.no>
2026-09-17 13:14:51 +0200

    Gjorde ferdig tre av funksjonene i oppgave-3. Ble ferdig med end_date som nå returnerer et ferdig klokkeslett. Gjorde også ferdig remaining_days som tar to datoer og returnerer antall dager mellom dem. Oppdaterte også main funksjonen til å kalle alle de andre funksjonene. Nå skal jeg jobbe videre med den siste funksjonen som skal ta imot en liste med datoer og returnere de kronologisk.

--- oppgave-3.py
+++ oppgave-3.py
@@ -1,5 +1,5 @@
 import datetime as dt
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # Ikke forventet en meny, bare funksjoner
 
@@ -9,19 +9,43 @@ def new_date(date):
             "%d.%m.%Y")
         return date
     except ValueError:
-        print("Invalid date format")
+        print("Invalid date format. Please try again!")
         return None
 
 def end_date(start_time, minutes):
-    print("In progress")
+    try:
+        start_time = dt.datetime.strptime(start_time,
+                                      "%H.%M")
+        try:
+            if minutes > 0 and minutes:
+                minutes = timedelta(minutes=minutes)
+            else:
+                return None
+        except ValueError:
+            print("Invalid input. Try again")
+            return None
+
+        end = start_time + minutes
+        return end
+
+    except ValueError:
+        print("Invalid time format. Please try again!")
+        return None
 
 def remaining_days(date1, date2):
-    if date2 > date1:
-        remaining = date2 - date1
-        return remaining.date
-    else:
-        remaining = date1 - date2
-        return remaining.date
+    try:
+        date1 = dt.datetime.strptime(date1,
+                                    "%d.%m.%Y")
+        date2 = dt.datetime.strptime(date2,
+                                    "%d.%m.%Y")
+        if date2 > date1:
+            remaining = date2 - date1
+            return remaining
+        else:
+            remaining = date1 - date2
+            return remaining
+    except ValueError:
+        print("Invalid input. Try again")
 
 def chronological_list(list_of_dates):
     print("In progress")
@@ -29,8 +53,12 @@ def chronological_list(list_of_dates):
 def main():
     while True:
         first_date = new_date(input("What is the date? "))
-        if first_date is not None:
-            print(first_date)
+        end_time = end_date(input("When did you start? "), int(input("How many minutes? ")))
+        remaining = remaining_days(input("What is the first date? "), input("What is the second date? "))
+        if first_date is not None and end_time is not None and remaining is not None:
+            print(datetime.strftime(first_date, "%d.%m.%Y"))
+            print(datetime.strftime(end_time, "%H.%M"))
+            print(f"{remaining.days} days between.")
             break
         else:
             pass
