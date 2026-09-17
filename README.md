1f6da205244b81154d52bf78602b69cf8448bdde
vicbro <vicbro@hotmail.no>
2026-09-17 14:05:15 +0200

    Gjorde ferdig siste funksjonen som lar en bruker skrive inn en liste med datoer, deretter så sorterer den datoene og skriver dem ut kronologisk. I tillegg så oppdaterte jeg readme filen slik at jeg dokumenterer alt jeg har gjort for oppgave-3.

--- README.md
+++ README.md
@@ -4,3 +4,63 @@
 Jeg valgte å strukturere dataene mine innenfor en liste som heter all_sessions.
 Innenfor denne listen har jeg 5 dictionaries for studie økter.
 Når brukeren da lager en ny økt, blir den lagret som en ny dictionary innenfor all_sessions listen.
+
+## Oppgave 3:
+Dokumentasjonens nettside jeg brukte var for det meste disse: 
+Tittel: datetime objects, link: https://docs.python.org/3/library/datetime.html#datetime-objects,
+Tittel: timedelta objects, link: https://docs.python.org/3/library/datetime.html#timedelta-objects,
+Tittel: date objects, link: https://docs.python.org/3/library/datetime.html#date-objects.
+I tillegg til at jeg brukte dokumentasjonen, brukte jeg også litt vanlige søkemotorer for å finne hjelp til 
+hvordan man skriver ulike date objekter, siden jeg syntes det var ganske avansert.
+
+Funksjon: new_date
+Input: 23.07.2000
+Gyldig resultat: 23.07.2000
+
+Funksjon: new_date
+Input: 31.12.2026
+Gyldig resultat: 31.12.2026
+
+Funksjon: new_date
+Input: 31.32.3200
+Ugyldig resultat: feilmelding "Invalid date format. Please try again!"
+
+Funksjon: end_date
+Input: 14.30 og 30
+Gyldig resultat: 15.00
+
+Funksjon: end_date
+Input: 30 og 14.30
+Ugyldig resultat: feilmelding "Invalid time format. Please try again!"
+
+Funksjon: end_date
+Input: hi og no
+Ugyldig resultat: feilmelding "Invalid time format. Please try again!"
+
+Funksjon: remaining_days
+Input: 23.07.2000 og 24.07.2000
+Gyldig resultat: 1 days between.
+
+Funksjon: remaining_days
+Input: 23.07.2000 og nei
+Ugyldig resultat: feilmelding "Invalid input. Try again"
+
+Funksjon: remaining_days
+Input: nei og nei
+Ugyldig resultat: feilmelding "Invalid input. Try again"
+
+Funksjon: chronological_list
+Input: 23.07.2000, 24.07.2000, 25.07.2000
+Gyldig resultat: 23.07.2000
+                 24.07.2000
+                 25.07.2000
+
+Funksjon: chronological_list
+Input: 25.07.2000, 24.07.2000, 23.07.2000
+Gyldig resultat: 23.07.2000
+                 24.07.2000
+                 25.07.2000
+
+Funksjon: chronological_list:
+Input: nei, 23.07.2000
+Ugyldig resultat: feilmelding "Invalid input. Try again"
--- oppgave-3.py
+++ oppgave-3.py
@@ -1,8 +1,6 @@
 import datetime as dt
 from datetime import datetime, timedelta
 
-# Ikke forventet en meny, bare funksjoner
-
 def new_date(date):
     try:
         date = dt.datetime.strptime(date,
@@ -17,9 +15,11 @@ def end_date(start_time, minutes):
         start_time = dt.datetime.strptime(start_time,
                                       "%H.%M")
         try:
+            minutes = int(minutes)
             if minutes > 0 and minutes:
                 minutes = timedelta(minutes=minutes)
             else:
+                print("Not a positive minute number. Try again")
                 return None
         except ValueError:
             print("Invalid input. Try again")
@@ -48,19 +48,43 @@ def remaining_days(date1, date2):
         print("Invalid input. Try again")
 
 def chronological_list(list_of_dates):
-    print("In progress")
+    dates = []
+    lists = list_of_dates.split(", ")
+
+    for date in lists:
+        try:
+            list_date = dt.datetime.strptime(date,
+                                        "%d.%m.%Y")
+            dates.append(list_date)
+        except ValueError:
+            print("Invalid input. Try again")
+            return None
+
+    return sorted(dates)
 
 def main():
     while True:
         first_date = new_date(input("What is the date? "))
-        end_time = end_date(input("When did you start? "), int(input("How many minutes? ")))
-        remaining = remaining_days(input("What is the first date? "), input("What is the second date? "))
-        if first_date is not None and end_time is not None and remaining is not None:
+        if first_date is not None:
             print(datetime.strftime(first_date, "%d.%m.%Y"))
+            break
+    while True:
+        end_time = end_date(input("When did you start? "), input("How many minutes? "))
+        if end_time is not None and end_time:
             print(datetime.strftime(end_time, "%H.%M"))
+            break
+    while True:
+        remaining = remaining_days(input("What is the first date? "), input("What is the second date? "))
+        if remaining is not None:
             print(f"{remaining.days} days between.")
             break
-        else:
-            pass
+    while True:
+        dates = chronological_list(input("Give a list of dates (separated by commas) "))
+        if dates is not None:
+            for date in dates:
+                final_date = datetime.strftime(date, "%d.%m.%Y")
+                print(final_date)
+            break
+
 if __name__ == "__main__":
     main()
