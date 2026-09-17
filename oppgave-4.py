7ada6ca03cdbe283de3d28cb2bd1b35f5f7713be
vicbro <vicbro@hotmail.no>
2026-09-17 23:13:41 +0200

    Arbeidet mye med oppgave-4. Filen blir lest av og fyller kravene til del 2 av oppgaven. Alle felt har verdi, id er et positivt tall, minutes er et helt tall, og is_resolved er yes eller no. Terminalen viser ugyldig melding for de radene som ikke valideres. Videre så vises det i terminalen: antall gyldige henvendelser og antall i hver kategori, samlet og gjennomsnittlig tidsbruk, med en desimal, antall løste og uløste henvendelser, kategorien med flest henvendelser og uløste henvendelser sorter fra lengst til kortest. Videre skal jeg fortsette med oppgave-4 for å lage en txt fil.

---
+++ oppgave-4.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+import csv
+
+print(f"Current directory is: {Path.cwd()}")
+data_directory = Path(".") / "supporthenvendelser.csv"
+support_path = Path(".") / "supporthenvendelser.csv"
+rapport_path = Path(".") / "support_rapport.txt"
+
+print(f"Directory exists: {data_directory.exists()}")
+print(support_path)
+print(rapport_path)
+
+valid = 0
+category_count = {}
+resolved_count = {}
+unresolved_count = []
+total = 0
+
+# Checks if row is empty
+with open("supporthenvendelser.csv", "r", encoding="utf-8") as f:
+    data = csv.reader(f)
+    next(data)
+    for row_number, row in enumerate(data):
+        # Starts the row from row 2
+        row_number += 1
+        if "" in row or len(row) != 4:
+            print(f"Line {row_number} id: {row[0]} is missing a value")
+            continue
+        int_row = int(row[0])
+        if int_row < 0:
+            print(f"Line {row_number} id: {row[0]} is not a positive whole number")
+            continue
+        int_minutes = row[2]
+        try:
+            int_minutes = int(int_minutes)
+        except ValueError:
+            print(f"Line {row_number} id: {row[0]} does not include a number")
+            continue
+        # Checks if row has yes or no, if it does it adds 1 to all valid rows, and adds the total minutes
+        if row[3] == "yes" or row[3] == "no":
+            valid += 1
+            if row[1] in category_count:
+                category_count[row[1]] += 1
+            else:
+                category_count[row[1]] = 1
+            total += int_minutes
+            if row[3] in resolved_count:
+                resolved_count[row[3]] += 1
+            else:
+                resolved_count[row[3]] = 1
+            if row[3] == "no":
+                unresolved_count.append(row)
+        else:
+            print(f"Line {row_number} id: {row[0]} is not 'yes' or 'no'")
+        # Shows the entire row
+        print(row)
+        # Shows total minutes in minutes category
+        print(f"Total minutes are: {total}")
+        average = total / valid
+        # Shows average minutes from valid rows
+        print(f"Average minutes are: {average:.1f}")
+
+
+    # Shows how many values are in each category
+    for c, category in category_count.items():
+        print(f"Category: {c}: {category}")
+    # Shows how many valid rows there are
+    print(f"Number of valid rows are: {valid}")
+
+    # Shows how many resolved and unresolved issues there are
+    for r, resolved in resolved_count.items():
+        if r == "yes":
+            print(f"Resolved issues are: {resolved}")
+        else:
+            print(f"Unresolved issues are: {resolved}")
+
+    # Shows the category with the most issues
+    highest_value = max(category_count, key=category_count.get)
+    print(f"Category with the most issues is: {highest_value}")
+
+    # Shows sorted unresolved issues
+    print(f"Unresolved issues from longest to shortest: {sorted(unresolved_count, key=lambda row: row[2], reverse=True)}")
