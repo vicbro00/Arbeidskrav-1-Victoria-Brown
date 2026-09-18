6c094c8e70b88aaa52fdebe0443af9db0fab06da
vicbro <vicbro@hotmail.no>
2026-09-18 14:37:04 +0200

    Begynte på oppgave 5 og laget en skisse for aktivitet dictionary. I tillegg lagde jeg menyen som skal vises i programmet.

---
+++ oppgave-5.py
@@ -0,0 +1,19 @@
+activity = [
+    {"title": "Football",
+     "category": "Sports",
+     "date": "14.12.2026",
+     "estimated_minutes": 45,
+     "status": "Planned"}
+]
+
+# Menu
+while True:
+    print("1. Register and show activities")
+    print("2. Search for title or category")
+    print("3. Filter by status")
+    print("4. Sort by date or duration")
+    print("5. Mark activity as completed")
+    print("6. Show number of activities, total estimated time and number of completed activities")
+    print("7. Save activities to file and read them again")
+    print("8. Quit program")
+    break
